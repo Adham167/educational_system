e31d4ff57ab3f3529591a247d50cf253db8c1142
Adham167 <adhamabdelsalam3040@gmail.com>
2025-08-14 07:23:30 +0300

    seting environment part_1

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesLogo1
+  /// assets/images/logo_1.png
+  static const String imagesLogo1 = "assets/images/logo_1.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding_1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding_1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding_2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding_2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding_3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding_3.png";
+
+  /// Assets for imagesOnboarding4
+  /// assets/images/onboarding_4.png
+  static const String imagesOnboarding4 = "assets/images/onboarding_4.png";
+}
+
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const styleSemiBold35 = TextStyle(
+    color: Color(0xFF022D4F),
+    fontSize: 35,
+    fontFamily: "Plus Jakarta Sans",
+    fontWeight: FontWeight.w600
+    );
+}
